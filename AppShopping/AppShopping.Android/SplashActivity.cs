59795df2498ff5615736654cd57fb568e98739bc
Luiz Chequini <46603262+luizchequini@users.noreply.github.com>
2021-08-30 11:46:37 -0300

    Android - SplashScrem

--- AppShopping/AppShopping.Android/SplashActivity.cs
+++ AppShopping/AppShopping.Android/SplashActivity.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -11,7 +12,7 @@ using System.Text;
 
 namespace AppShopping.Droid
 {
-    [Activity(Label = "App - Quantidade", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "App-Shopping", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class SplashActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- AppShopping/AppShopping/Menu.xaml.cs
+++ AppShopping/AppShopping/Menu.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace AppShopping
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : ContentPage
     {
-        public MainPage()
+        public Menu()
         {
             InitializeComponent();
         }
