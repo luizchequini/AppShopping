4bef13f7355bb20781d98bbaa9cac53a99277eb3
Luiz Chequini <46603262+luizchequini@users.noreply.github.com>
2021-08-30 19:48:33 -0300

    StoresViewModel - Parte 1 - Propriedades e Construtor

--- AppShopping/AppShopping/ViewModels/StoresViewModel.cs
+++ AppShopping/AppShopping/ViewModels/StoresViewModel.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using AppShopping.Library.Enuns;
+using AppShopping.Models;
+using AppShopping.Services;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace AppShopping.ViewModels
 {
     public class StoresViewModel
     {
+        public string SearchWord { get; set; }
+        public ICommand SearchCommand { get; set; }
+        public List<Establishment> Establishments { get; set; }
 
+        public StoresViewModel()
+        {
+            SearchCommand = new Command(Search);
+            
+            var allEstablishments = new EstablishmentService().GetEstablishments();
+            var allStores = allEstablishments.Where(a => a.Type == EstablishmentType.Store).ToList();
+
+            Establishments = allStores;
+        }
+
+        private void Search()
+        {
+
+        }
     }
 }
