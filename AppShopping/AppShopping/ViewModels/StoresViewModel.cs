38f5b95d37b1e71592abee907d481e353d884265
Luiz Chequini <46603262+luizchequini@users.noreply.github.com>
2021-08-30 23:12:59 -0300

    View Stores Estilo CollectionView

--- AppShopping/AppShopping/ViewModels/StoresViewModel.cs
+++ AppShopping/AppShopping/ViewModels/StoresViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using AppShopping.Library.Enuns;
+using AppShopping.Library.Helpers.MVVM;
 using AppShopping.Models;
 using AppShopping.Services;
 using System;
@@ -10,25 +11,40 @@ using Xamarin.Forms;
 
 namespace AppShopping.ViewModels
 {
-    public class StoresViewModel
+    public class StoresViewModel : BaseViewModel
     {
         public string SearchWord { get; set; }
         public ICommand SearchCommand { get; set; }
-        public List<Establishment> Establishments { get; set; }
+
+        private List<Establishment> _establishments;
+
+        public List<Establishment> Establishments {
+            get
+            {
+                return _establishments;
+            }
+            set 
+            {
+               SetProperty(ref _establishments, value);
+            } 
+        }
+
+        private List<Establishment> _allEstablishments;
 
         public StoresViewModel()
         {
             SearchCommand = new Command(Search);
-            
+
             var allEstablishments = new EstablishmentService().GetEstablishments();
             var allStores = allEstablishments.Where(a => a.Type == EstablishmentType.Store).ToList();
 
             Establishments = allStores;
+            _allEstablishments = allStores;
         }
 
         private void Search()
         {
-
+            Establishments = _allEstablishments.Where(a => a.Name.ToLower().Contains(SearchWord.ToLower())).ToList();
         }
     }
 }
