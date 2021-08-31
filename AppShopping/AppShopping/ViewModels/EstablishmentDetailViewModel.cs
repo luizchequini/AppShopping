3f14f6f3a4c376dd671b1ff19e3abf72523af37e
Luiz Chequini <46603262+luizchequini@users.noreply.github.com>
2021-08-31 15:47:52 -0300

    View - Establishment Detail - Estilo - Parte 1 e 2

---
+++ AppShopping/AppShopping/ViewModels/EstablishmentDetailViewModel.cs
@@ -0,0 +1,20 @@
+﻿using AppShopping.Library.Helpers.MVVM;
+using AppShopping.Models;
+using AppShopping.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppShopping.ViewModels
+{
+    public class EstablishmentDetailViewModel : BaseViewModel
+    {
+        public Establishment Establishment { get; set; }
+
+        public EstablishmentDetailViewModel()
+        {
+            Establishment = new EstablishmentService().GetEstablishments().First();
+        }
+    }
+}
